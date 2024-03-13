a934b3657f46b4361c6677a724021c6e3d8cafe7
Ronan Anacleto <ronan.anacletolopes@gmail.com>
2024-03-13 11:06:46 -0300

    Projeto de Exemplo

--- App.js
+++ App.js
@@ -1,7 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+//add
+import config from "./src/database/config";
+import { createConnection } from "typeorm";
+
+
 export default function App() {
+
+  // add a useCallback hook
+  const connect = React.useCallback(async () => {
+    try {
+      const connection = await createConnection(config); // create a connection with our config
+    } catch (err) {
+      console.log(err); // check (or deal) with connection errors
+    }
+  });
+
+  // Important: connect inside a dependency-free useEffect hook to avoid multiple calls.
+  React.useEffect(() => {
+    connect(); // this must be done once in the App's entry point (I suggest here in App.js)
+  }, []);
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
